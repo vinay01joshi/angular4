a6abd8d24f5d7c5dba11b41668b598ff100d4c3c
Vinay Joshi <vinay01joshi@gmail.com>
2017-07-23 12:01:04 +0530

    ngFor directive implementation

--- src/app/courses/courses.component.ts
+++ src/app/courses/courses.component.ts
@@ -8,8 +8,16 @@ import { Component } from "@angular/core";
 })
 export class CoursesComponent {
     title = "List of courses";
-    courses;        
+    courses;
+    computerCourses = [
+        { id : 1 , name: 'AspNetCore' },
+        { id : 2 , name: 'Angular2' },
+        { id : 3 , name: 'Docker' },
+        { id : 4 , name: 'Microsoft Bot' },
+        { id : 5 , name: 'Electron Fundamental' }
+    ]        
     viewMode = "map";
+
     constructor(private _courseService : CoursesService){
         this.courses = this._courseService.getCourses()
     }
