9afce4a5fc33a12faad08cb81e1ebda87673d84a
Vinay Joshi <vinay01joshi@gmail.com>
2017-07-23 19:50:45 +0530

    Applying ngModel in form control

--- src/app/contact-form/contact-form.component.ts
+++ src/app/contact-form/contact-form.component.ts
@@ -5,11 +5,12 @@ import { Component, OnInit } from '@angular/core';
   templateUrl: './contact-form.component.html',
   styleUrls: ['./contact-form.component.css']
 })
-export class ContactFormComponent implements OnInit {
+export class ContactFormComponent  {
 
   constructor() { }
 
-  ngOnInit() {
+  log(x){
+    console.log(x);
   }
 
 }
