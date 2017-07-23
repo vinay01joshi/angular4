b07231ca2d056a75df2c1ba38503b0e39f3c70e9
Vinay Joshi <vinay01joshi@gmail.com>
2017-07-23 10:25:47 +0530

    Restructuring the courses component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,12 +1,10 @@
 import { SummaryPipe } from './shared/summary.pipe';
 import { AuthorService } from './author/author.service';
-import { CoursesService } from './courses.service';
 import { BrowserModule } from '@angular/platform-browser';
 import { FormsModule } from '@angular/forms';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
-import { CoursesComponent } from "./courses.component";
 import { AuthorComponent } from './author/author.component';
 import { BindingsComponent } from './bindings/bindings.component';
 import { FavoriteComponent } from './favorite/favorite.component';
@@ -14,6 +12,8 @@ import { TitleCaseComponent } from './title-case/title-case.component';
 import { TitleCasePipe } from './shared/title-case.pipe';
 import { PanelComponent } from './panel/panel.component';
 import { LikeComponent } from './like/like.component';
+import { CoursesComponent } from "./courses/courses.component";
+import { CoursesService } from "./courses/courses.service";
 
 @NgModule({
   declarations: [
