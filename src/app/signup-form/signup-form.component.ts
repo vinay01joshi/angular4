e92c0b658cab3da9871136b7ea1f705c3cf60ad2
Vinay Joshi <vinay01joshi@gmail.com>
2017-07-24 21:12:59 +0530

    Reactive form | Adding custom validator

--- src/app/signup-form/signup-form.component.ts
+++ src/app/signup-form/signup-form.component.ts
@@ -1,6 +1,6 @@
 import { Component } from '@angular/core';
-
 import { FormGroup, FormControl , Validators } from "@angular/forms";
+import { UserNameValidator } from './username.validators';
 
 @Component({
   selector: 'signup-form',
@@ -11,7 +11,8 @@ export class SignupFormComponent {
   form = new FormGroup({
       username : new FormControl('',[
         Validators.required,
-        Validators.minLength(3)
+        Validators.minLength(3),
+        UserNameValidator.cannotContainSpace
       ]),
       password : new FormControl('',Validators.required)
   });
