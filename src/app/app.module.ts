62c8cfafecfd7f141c94b3bed14a9383ce12e314
Vinay Joshi <vinay01joshi@gmail.com>
2017-07-25 08:37:56 +0530

    Reactive Forms | Confirm password exersize complete

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -20,6 +20,7 @@ import { ContactFormComponent } from './contact-form/contact-form.component';
 import { UdemyCourseComponent } from './udemy-course/udemy-course.component';
 import { SignupFormComponent } from './signup-form/signup-form.component';
 import { NewCourseFormComponent } from './new-course-form/new-course-form.component';
+import { ChangePasswordComponent } from './change-password/change-password.component';
 
 @NgModule({
   declarations: [
@@ -40,6 +41,7 @@ import { NewCourseFormComponent } from './new-course-form/new-course-form.compon
     UdemyCourseComponent,
     SignupFormComponent,
     NewCourseFormComponent,
+    ChangePasswordComponent,
   ],
   imports: [
     BrowserModule,
---
+++ src/app/change-password/change-password.component.ts
@@ -0,0 +1,29 @@
+import { PasswordValidators } from './password.validators';
+import { Validators } from '@angular/forms';
+import { FormGroup, FormBuilder } from '@angular/forms';
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'change-password',
+  templateUrl: './change-password.component.html',
+  styleUrls: ['./change-password.component.css']
+})
+export class ChangePasswordComponent  {
+  form : FormGroup ;
+
+  constructor(fb : FormBuilder){
+     this.form = fb.group({
+       oldPassword : [''
+          ,Validators.required
+          ,PasswordValidators.validOldPassword],
+       newPassword : ['',Validators.required],
+       confirmPassword : ['',Validators.required],       
+     },{
+       validator : PasswordValidators.passwordsShouldMatch
+     });
+  }
+
+  get oldPassword() { return this.form.get('oldPassword'); }
+  get newPassword() { return this.form.get('newPassword'); }
+  get confirmPassword() { return this.form.get('confirmPassword'); }
+}
