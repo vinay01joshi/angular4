69a7605d7231b046551a264eb212f801ee34361e
Vinay Joshi <vinay01joshi@gmail.com>
2017-07-28 08:58:19 +0530

    Routing and Navigation | Programmatic Navigation

--- src/app/github-profile/github-profile.component.ts
+++ src/app/github-profile/github-profile.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-import { ActivatedRoute } from "@angular/router";
+import { ActivatedRoute, Router } from "@angular/router";
 
 @Component({
   selector: 'github-profile',
@@ -8,18 +8,24 @@ import { ActivatedRoute } from "@angular/router";
 })
 export class GithubProfileComponent implements OnInit {
 
-  constructor(private route: ActivatedRoute) { }
+  constructor(private route: ActivatedRoute , private router : Router) { }
 
   ngOnInit() {
 
-    let id  = this.route.snapshot.paramMap.get('id');
-    console.log(id);
+    //let id  = this.route.snapshot.paramMap.get('id');
+    //console.log(id);
 
     // this.route.paramMap
     //   .subscribe(params => {
     //      let id = +params.get('id'); // '+' is for converting string to integer
     //      console.log(id);
     //   })
+  }
+
+  submit(){
+    this.router.navigate(['/followers'],{
+      queryParams: {page :1,order : 'newest'}
+    })
   }
 
 }
