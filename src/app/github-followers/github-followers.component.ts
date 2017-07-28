77e4470fe1949f4ff40461ea7667591423375d04
Vinay Joshi <vinay01joshi@gmail.com>
2017-07-28 08:47:47 +0530

    Routing and Navigation | The switchMap operator

--- src/app/github-followers/github-followers.component.ts
+++ src/app/github-followers/github-followers.component.ts
@@ -1,6 +1,8 @@
 import { Component, OnInit } from '@angular/core';
 import { Observable } from 'rxjs/Observable';
 import 'rxjs/add/observable/combineLatest';
+import 'rxjs/add/operator/map';
+import 'rxjs/add/operator/switchMap';
 
 import { ActivatedRoute } from '@angular/router';
 import { GithubFollowersService } from './../github-followers.service';
@@ -19,15 +21,14 @@ export class GithubFollowersComponent implements OnInit {
     Observable.combineLatest([
       this.route.paramMap,
       this.route.queryParamMap
-    ]).subscribe(combined =>{
-        let id = combined[0].get('id');
-        let page = combined[1].get('page');
-        this.service.getAll()
-            .subscribe(followers => this.followers = followers);
+    ])
+    .switchMap(combined => {
+      let id = combined[0].get('id');
+      let page = combined[1].get('page');
+      return this.service.getAll();        
     })
-
-
-   
+    .subscribe(followers => this.followers = followers);
+    
   }
 
 }
