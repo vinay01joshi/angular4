3e054e77af6be3098b83fc4a980d0774fc123cf7
Vinay Joshi <vinay01joshi@gmail.com>
2017-08-18 10:19:23 +0530

    Animations | Sate for implementing clean coding

--- src/app/posts/posts.component.ts
+++ src/app/posts/posts.component.ts
@@ -12,13 +12,14 @@ import { trigger, state, transition, style, animate } from "@angular/animations"
   animations:[
     trigger('fade',[
 
+      state('void',style({opacity:0})),
+
       transition('void => *',[
-        style({opacity:0}),
         animate(2000)
       ]),
 
       transition('* => void',[
-        animate(2000,style({opacity:0}))
+        animate(2000)
       ])
     ])
   ]
