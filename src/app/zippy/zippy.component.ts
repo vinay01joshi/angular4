cbd63b4c61d03531a5b038ee8287ff41532e204f
Vinay Joshi <vinay01joshi@gmail.com>
2017-08-20 19:23:45 +0530

    Animations | Separation of Concerns

--- src/app/zippy/zippy.component.ts
+++ src/app/zippy/zippy.component.ts
@@ -1,33 +1,11 @@
+import { expandCollapse } from './../shared/animations/zippy.component.animations';
 import { Component, OnInit, Input } from '@angular/core';
-import { trigger, state, transition, style, animate, keyframes, useAnimation, query, animateChild, group } from "@angular/animations";
 
 @Component({
   selector: 'zippy',
   templateUrl: './zippy.component.html',
   styleUrls: ['./zippy.component.css'],
-  animations:[
-    trigger('expandCollapse',[
-      state('collapsed',style({
-        height: 0,
-        paddingTop: 0,
-        paddingBottom: 0,
-        opacity: 0    
-      })),
-      transition('collapsed => expanded',[
-        animate('300ms ease-out',style({
-          height: '*',
-          paddingTop: '*',
-          paddingBottom: '*'
-        })),
-        animate(1000,style({
-          opacity: 1
-        }))
-      ]),
-      transition('expanded => collapsed',[
-        animate('300ms ease-in')
-      ])
-    ])
-  ]
+  animations:[expandCollapse]
 })
 export class ZippyComponent {
 
